b405ed29f33898e94b8f857b26025d656a70e9d9
Boris Karaoglanov <bkaraoglanov@astraflare.com>
2020-03-17 09:54:41 -0700

    Changing bucket name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "${var.project_name}-${var.environment}-${var.region}-core-state"
+  bucket = "${company_name}-${var.region}-aws-core-tf-state"
   acl    = var.acl
 
   versioning {
